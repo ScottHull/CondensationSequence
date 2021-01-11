7af7c646507c3bb7186428e6de4dcc794294d77d
ScottHull <scottdhull21@gmail.com>
2021-01-11 13:19:39 -0500

    initial commit.

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+class Condensation:
+
+    def __init__(self):
+        self.start_temperature = 2000
+        self.end_temperature = 200
+        self.dt = 2
+        self.total_pressure = 1
+        self.abundances = {}
+        self.normalized_abundances = {}
+
+
