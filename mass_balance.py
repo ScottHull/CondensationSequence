a3b0b0634c5f08e797fe90d4921ee0e1dffb6f78
ScottHull <scottdhull21@gmail.com>
2021-01-18 23:43:02 -0500

    Work in progress.

--- mass_balance.py
+++ mass_balance.py
@@ -46,54 +46,74 @@ def get_equations(condensing_solids):
     return Molecule_dict, X, Element_dict, Z
 
 
-def mass_balance(element_appearances, K_dict, partial_pressures, gasses, temperature, condensing_solids):
-    # note that element appearances should be a dict where the cation/anion is the key and a list of molecules that it appears in is the value
+def Mass_balance_fun(guess_list, *args):
+    # Element_dict is a dictionary of elements from given abundances and all of the gas molecules in which it appears
+    # K is the dictionary of solid and gas K values
+    # Par_Pres is a dictionary of partial pressures of given abundance elements
+    # gasses is a dictionary of gasses and their corresponding stoichiometry
+    # condensing_solids is a list of condensing solids
+    # guess is the same as Par_Pres but in list form and corresponds to the list of inputted elements (note that these values may reference and older Par_pres dict?)
     fugacities = ['H', 'Cl', 'F', 'O', 'N']
-    R = 8.3144621e-2  # units of L bar mol^-1 K^-1
-
-    RT = R * temperature
+    R = 8.3144621e-2
+    # units of L bar mol^-1 K^-1
+    Element_dict = args[0]
+    K = args[1]
+    Par_Pres = args[2]
+    gasses = args[3]
+    Name = args[4]
+    RT = R * args[5]
+    condensing_solids = args[6]
     guess_dict = {}
-    for key in partial_pressures.keys():
-        guess_dict.update({key: 0})
 
+    test = {}
+    counter = 0
+    for i in Name:  # loop through given input elements list
+        guess_dict[i] = guess_list[counter]  # build a guess dict with the element as the key and the (old) partial pressure as the value
+        counter += 1
+
+    final = []  # Sum of all activity terms for individual element
+    outs = 1
+    out = []
     final_dict = {}
     finals = []
+    coefs = {}
     out_dict = {}
     # print guess_dict
-    for element in element_appearances.keys():
-        out = 0
-        for molecule in element_appearances[element]:
+    for i, j in dict.iteritems(Element_dict):  # i is element, j is list of molecules in which it exists
+        out = 0.
+        for k in j:  # for molecule in the list of molecules that contains k
             # print k
-            coefs = gasses[molecule]  # open an individual gas species
-            entry = 0.0  # what is this?
-            for atom in coefs:
-                stoich_coeff = coefs[atom]
-                if guess_dict[atom] <= 0:  # if the partial pressure becomes negative
+            coefs = gasses.get(k)  # get the number of atoms in the gas species
+            entry = 0.
+            for x, y in dict.iteritems(coefs):  # x is the atom in the molecule, y is the atom's stoichiometry
+                if guess_dict[x] <= 0.:  # if the partial pressure of the atom is < 0
                     entry = 1.e999
                     break
                 else:
-                    if atom == element:
-                        if atom in fugacities:
-                            entry = entry + ((stoich_coeff / 2.0) * log10(partial_pressures[atom] * RT) + log10(
-                                stoich_coeff))
+                    # these look like the mass balance equations for gasses given by Mg/Si paper
+                    if x == i:  # if the atom from the molecule equals in the input atom
+                        if x in fugacities:  # if the atom is in the fugacities list (2 from diatomic molecules?)
+                            entry = entry + ((y / 2.) * log10(guess_dict.get(x) * RT) + log10(y))
                         else:
-                            entry = entry + (stoich_coeff * log10(partial_pressures[atom] * RT) + log10(stoich_coeff))
+                            # this is a summation: sum(nu * log(P_i * RT) * log(nu)) = sum(nu * log(nu * P_i + nu * RT)
+                            entry = entry + (y * log10(guess_dict.get(x) * RT) + log10(y))
                     else:
-                        if atom in fugacities:
-                            entry = entry + ((stoich_coeff / 2.0) * log10(partial_pressures[atom] * RT))
+                        if x in fugacities:
+                            entry = entry + ((y / 2.) * log10(guess_dict.get(x) * RT))
                         else:
-                            entry = entry + (stoich_coeff * log10(partial_pressures[atom] * RT))
-            entry = 10 ** (entry + (K_dict[molecule]) - log10(RT))
+                            entry = entry + (y * log10(guess_dict.get(x) * RT))
+            entry = entry + (K.get(k)) - log10(RT)
+            entry = pow(10., entry)
             out += entry
-        out_dict.update({element: out})
 
-    for element in partial_pressures.keys():
-        pp = partial_pressures[element]  # get the partial pressure value of the element
-        final_dict.update({element: log10(pp) - log10(out_dict[element])})
+        out_dict.update({i: out})
+    for i, j in dict.iteritems(Par_Pres):
+        final_dict.update({i: log10(j) - log10(out_dict.get(i))})
 
     if len(condensing_solids) > 0:
-        solid_molecule_dict, Y, solid_element_dict, Z = get_equations(condensing_solids)
-        for molecule in solid_element_dict:  # open an element
+
+        solid_molecule_dict, Y, solid_element_dict, Z = get_data.get_equations(condensing_solids)
+        for i, j in dict.iteritems(solid_element_dict):  # open an element
             Sum = 0.
             for k in j:  # open molecule that contains that element
                 if guess_dict[k] <= 0.:
@@ -107,4 +127,34 @@ def mass_balance(element_appearances, K_dict, partial_pressures, gasses, tempera
                             else:
                                 Sum += float(x[1]) * guess_dict[k]
 
-            final_dict.update({i: 1. - ((log10(out_dict[i] + Sum) / log10(partial_pressures[i])))})
+            final_dict.update({i: 1. - ((log10(out_dict[i] + Sum) / log10(Par_Pres.get(i))))})
+
+    counter = 0
+    for i in Name:
+        if i not in condensing_solids:
+            finals.append(final_dict.get(i))
+    # condensing_solids = ['Al2O3']
+    # solid_molecule_dict,Y,solid_element_dict,Z = get_data.get_equations(condensing_solids)
+    if len(condensing_solids) > 0:
+
+        for i, j in dict.iteritems(solid_molecule_dict):
+
+            pressures = 0.
+            for k in j:
+                if guess_dict.get(k[0]) < 0.:
+                    pressures = 1.e999
+                    break
+                else:
+                    if k[0] in fugacities:
+                        pressures += (float(k[1]) / 2.) * (log10(guess_dict.get(k[0]) * RT))
+                    else:
+                        pressures += (float(k[1])) * (log10(guess_dict.get(k[0]) * RT))
+
+            if K.get(i) == 0.:
+                finals.append(-K.get(i) - pressures)
+            else:
+                finals.append((1. - (pressures / -K.get(i))))
+
+    # Check whether solid equilibria even matters...
+
+    return finals
